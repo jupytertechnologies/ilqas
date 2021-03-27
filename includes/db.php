5d55033f678ef69b4387462875f395f4fbcc63b3
Andrew Kezy <kezyandrew@gmail.com>
2021-03-28 01:13:19 +0300

    participating lab layout edit

--- includes/db.php
+++ includes/db.php
@@ -8,7 +8,7 @@ Class Db {
 	private $dbname;
 	
 	protected function connect(){
-		$this->servername = "localhost";
+		$this->servername = "127.0.1.1";
 		$this->username = "root";
 		$this->password = "";
 		$this->dbname = "ilqas";
@@ -16,6 +16,7 @@ Class Db {
 		$conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
 		
 		return $conn;
+		
 	}
 }
 ?>
